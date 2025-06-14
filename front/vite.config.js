65f0d0a2f05972adf4ce130cca55fbeeff003be0
millslogan <ppa9ed@virginia.edu>
2025-06-13 20:59:39 -0400

    Add base name to config

--- front/vite.config.js
+++ front/vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
     root: './src',
     outDir: '../docs',
     emptyOutDir: true,
+    base: 'hole-punchle',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
