51ebd48e9cfc180c368ec666f38ff31f64d25f2c
MillsLogan <MillsLogan@users.noreply.github.com>
2025-06-14 00:49:47 +0000

    deploy: 7aa15c4b716d6cee09d948716064892a3aa87045

--- front/src/components/PunchControls/PunchButton.jsx
+++
@@ -1,9 +0,0 @@
-import "./PunchButton.css";
-
-export default function PunchButton({ punchFunction, disable }) {
-    return (
-        <div className="col-3 punch-button-container d-flex justify-content-center align-items-center">
-            <button className="punch-button" onClick={() => punchFunction()} disabled={disable()}></button>
-        </div>
-    )
-}
