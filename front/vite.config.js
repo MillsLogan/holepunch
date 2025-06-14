7aa15c4b716d6cee09d948716064892a3aa87045
millslogan <ppa9ed@virginia.edu>
2025-06-13 20:48:14 -0400

    Remove 404 from vite config

--- front/vite.config.js
+++ front/vite.config.js
@@ -12,7 +12,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        notFound: resolve(__dirname, '404.html')
       }
     }
   }
